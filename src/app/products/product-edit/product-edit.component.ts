ae37f9daa6722092db529805f9c02c658afdb761
Vipul Daripkar <vipuldaripkar@gmail.com>
2020-08-08 14:31:37 +0530

    Added generic and number validators

--- src/app/products/product-edit/product-edit.component.ts
+++ src/app/products/product-edit/product-edit.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { Subscription } from 'rxjs';
+
+import { ProductService } from '../product.service';
+import { GenericValidator } from '../../shared/generic-validator';
 
 @Component({
   selector: 'app-product-edit',
-  templateUrl: './product-edit.component.html',
-  styleUrls: ['./product-edit.component.css']
+  templateUrl: './product-edit.component.html'
 })
 export class ProductEditComponent implements OnInit {
+  pageTitle = 'Product Edit';
+  errorMessage = '';
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private productService: ProductService) {
+
+    // Defines all of the validation messages for the form.
+    // These could instead be retrieved from a file or database.
+
+    this.validationMessages = {
+      productName: {
+        required: 'Product name is required.',
+        minLength: 'Product name must be at least three characters.',
+        maxLength: 'Product name cannot exceed 50 characters.'
+      },
+      productCode: {
+        required: 'Product code is required.'
+      },
+      starRating: {
+        range: 'Rate the product between 1 (lowest) and 5 (highest).'
+      }
+    };
+
+    // Define an instance of the validator for use with this form,
+    // passing in this form's set of validation messages.
+    this.genericValidator = new GenericValidator(this.validationMessages);
+  }
 
   ngOnInit(): void {
+    // Define the form group
+    this.productForm = this.fb.group({
+      productName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(50)]],
+      productCode: ['', Validators.required],
+      starRating: ['', NumberValidators.range(1,5)],
+      description: ''
+    });
   }
 
 }
---
+++ src/app/shared/number.validator.ts
@@ -0,0 +1,8 @@
+import { AbstractControl, ValidatorFn } from '@angular/forms';
+
+export class NumberValidators {
+
+  static range(min: number, max: number): ValidatorFn {
+    
+  }
+}
